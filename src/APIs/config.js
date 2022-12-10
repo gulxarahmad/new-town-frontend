dd74b494e36accf4f0efdcb3e374eb9b20e93786
Gulzar Ahmad <gulzar@conovoinc.com>
2022-12-10 14:46:32 +0500

    api URL changed

--- src/APIs/config.js
+++ src/APIs/config.js
@@ -1,4 +1,4 @@
 //export const apiURL = "http://localhost:3009";
 // export const apiURL = "https://new-town-backend.herokuapp.com";
 
-export const apiURL = "ttps://newtown-backend.onrender.com"
+export const apiURL = "https://newtown-backend.onrender.com"
